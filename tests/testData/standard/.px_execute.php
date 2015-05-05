79e58fe49549dbaacbb7659a9fdd87aff23ae71b
Tomoya Koyanagi <tomk79@gmail.com>
2015-05-06 07:21:51 +0900

    コマンドラインから実行した場合の不具合を修正。
    - ドキュメントルート以外のディレクトリから相対パスで実行した場合に、スクリプトファイル名が取得できないことによる問題を解決。

--- tests/testData/standard/.px_execute.php
+++ tests/testData/standard/.px_execute.php
@@ -1,5 +1,9 @@
 <?php
+$_SERVER['PHP_SELF'] = __FILE__;
+$_SERVER['SCRIPT_NAME'] = __FILE__;
+$_SERVER['SCRIPT_FILENAME'] = __FILE__;
 chdir(__DIR__);
+
 @require_once( './../../../vendor/autoload.php' );
 @require_once( './px-files/themes/pickles/php/theme.php' );
 new picklesFramework2\pickles('./px-files/');
