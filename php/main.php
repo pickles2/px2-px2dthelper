2962ab51fd687b989b38b6a1fbf771bc16234195
Tomoya Koyanagi <tomk79@gmail.com>
2015-06-08 00:57:28 +0900

    API `PX=px2dthelper.convert_table_excel2html` を追加 (beta)

--- php/main.php
+++ php/main.php
@@ -144,7 +144,21 @@ class main{
 		switch( @$this->command[1] ){
 			case 'ping':
 				// 疎通確認応答
-				$this->fnc_ping();
+				@header('Content-type: text/plain;');
+				print 'ok'."\n";
+				exit;
+				break;
+			case 'convert_table_excel2html':
+				$path_xlsx = $this->px->req()->get_param('path');
+				if( !is_file($path_xlsx) || !is_readable($path_xlsx) ){
+					print $this->data_convert( false );
+					exit;
+					break;
+				}
+				$excel2html = new \tomk79\excel2html\main($path_xlsx);
+				$val = $excel2html->get_html();
+				print $this->data_convert( $val );
+				exit;
 				break;
 		}
 
@@ -154,15 +168,207 @@ class main{
 		exit;
 	}
 
+	// -------------------------------------
+
 	/**
-	 * 疎通確認応答
+	 * データを自動的に加工して返す。
 	 * 
-	 * @return void
+	 * @param mixed $val 加工するデータ
+	 * @return string 加工されたテキストデータ
 	 */
-	private function fnc_ping(){
-		header('Content-type: text/plain;');
-		print 'ok'."\n";
-		exit;
+	private function data_convert($val){
+		$data_type = $this->px->req()->get_param('type');
+		if( !is_string($data_type) || !strlen($data_type) ){
+			$data_type = 'json';
+		}
+		header('Content-type: application/xml; charset=UTF-8');
+		if( $data_type == 'json' ){
+			header('Content-type: application/json; charset=UTF-8');
+		}elseif( $data_type == 'jsonp' ){
+			header('Content-type: application/javascript; charset=UTF-8');
+		}
+		switch( $data_type ){
+			case 'jsonp':
+				return $this->data2jsonp($val);
+				break;
+			case 'json':
+				return $this->data2json($val);
+				break;
+			case 'xml':
+				return $this->data2xml($val);
+				break;
+		}
+		// return self::data2jssrc($val);
+		return json_encode($val);
+	}
+
+	/**
+	 * データをXMLに加工して返す。
+	 * 
+	 * @param mixed $val 加工するデータ
+	 * @return string 加工されたテキストデータ
+	 */
+	private function data2xml($val){
+		return '<api>'.self::xml_encode($val).'</api>';
+	}
+
+	/**
+	 * データをJSONに加工して返す。
+	 * 
+	 * @param mixed $val 加工するデータ
+	 * @return string 加工されたテキストデータ
+	 */
+	private function data2json($val){
+		// return self::data2jssrc($val);
+		return json_encode($val);
+	}
+
+	/**
+	 * データをJSONPに加工して返す。
+	 * 
+	 * @param mixed $val 加工するデータ
+	 * @return string 加工されたテキストデータ
+	 */
+	private function data2jsonp($val){
+		//JSONPのコールバック関数名は、パラメータ callback に受け取る。
+		$cb = trim( $this->px->req()->get_param('callback') );
+		if( !strlen($cb) ){
+			$cb = 'callback';
+		}
+		// return $cb.'('.self::data2jssrc($val).');';
+		return $cb.'('.json_encode($val).');';
+	}
+
+
+	/**
+	 * 変数をXML構造に変換する
+	 * 
+	 * @param mixed $value 値
+	 * @param array $options オプション
+	 * <dl>
+	 *   <dt>delete_arrayelm_if_null</dt>
+	 *     <dd>配列の要素が `null` だった場合に削除。</dd>
+	 *   <dt>array_break</dt>
+	 *     <dd>配列に適当なところで改行を入れる。</dd>
+	 * </dl>
+	 * @return string XMLシンタックスに変換された値
+	 */
+	private static function xml_encode( $value = null , $options = array() ){
+
+		if( is_array( $value ) ){
+			#	配列
+			$is_hash = false;
+			$i = 0;
+			foreach( $value as $key=>$val ){
+				#	ArrayかHashか見極める
+				if( !is_int( $key ) ){
+					$is_hash = true;
+					break;
+				}
+				if( $key != $i ){
+					#	順番通りに並んでなかったらHash とする。
+					$is_hash = true;
+					break;
+				}
+				$i ++;
+			}
+
+			if( $is_hash ){
+				$RTN .= '<object>';
+			}else{
+				$RTN .= '<array>';
+			}
+			if( $options['array_break'] ){ $RTN .= "\n"; }
+			foreach( $value as $key=>$val ){
+				if( $options['delete_arrayelm_if_null'] && is_null( $value[$key] ) ){
+					#	配列のnull要素を削除するオプションが有効だった場合
+					continue;
+				}
+				$RTN .= '<element';
+				if( $is_hash ){
+					$RTN .= ' name="'.htmlspecialchars( $key ).'"';
+				}
+				$RTN .= '>';
+				$RTN .= self::xml_encode( $value[$key] , $options );
+				$RTN .= '</element>';
+				if( $options['array_break'] ){ $RTN .= "\n"; }
+			}
+			if( $is_hash ){
+				$RTN .= '</object>';
+			}else{
+				$RTN .= '</array>';
+			}
+			if( $options['array_break'] ){ $RTN .= "\n"; }
+			return	$RTN;
+		}
+
+		if( is_object( $value ) ){
+			#	オブジェクト型
+			$RTN = '';
+			$RTN .= '<object>';
+			$proparray = get_object_vars( $value );
+			$methodarray = get_class_methods( get_class( $value ) );
+			foreach( $proparray as $key=>$val ){
+				$RTN .= '<element name="'.htmlspecialchars( $key ).'">';
+
+				$RTN .= self::xml_encode( $val , $options );
+				$RTN .= '</element>';
+			}
+			$RTN .= '</object>';
+			return	$RTN;
+		}
+
+		if( is_int( $value ) ){
+			#	数値
+			$RTN = '<value type="int">'.htmlspecialchars( $value ).'</value>';
+			return	$RTN;
+		}
+
+		if( is_float( $value ) ){
+			#	浮動小数点
+			$RTN = '<value type="float">'.htmlspecialchars( $value ).'</value>';
+			return	$RTN;
+		}
+
+		if( is_string( $value ) ){
+			#	文字列型
+			$RTN = '<value type="string">'.htmlspecialchars( $value ).'</value>';
+			return	$RTN;
+		}
+
+		if( is_null( $value ) ){
+			#	ヌル
+			return	'<value type="null"></value>';
+		}
+
+		if( is_resource( $value ) ){
+			#	リソース型
+			return	'<value type="undefined"></value>';
+		}
+
+		if( is_bool( $value ) ){
+			#	ブール型
+			if( $value ){
+				return	'<value type="bool">true</value>';
+			}else{
+				return	'<value type="bool">false</value>';
+			}
+		}
+
+		return	'<value type="undefined"></value>';
+
+	}
+
+	/**
+	 * ダブルクオートで囲えるようにエスケープ処理する。
+	 *
+	 * @param string $text テキスト
+	 * @return string エスケープされたテキスト
+	 */
+	private static function escape_doublequote( $text ){
+		$text = preg_replace( '/\\\\/' , '\\\\\\\\' , $text);
+		$text = preg_replace( '/"/' , '\\"' , $text);
+		return	$text;
 	}
 
 }
--- tests/px2-px2dthelperTest.php
+++ tests/px2-px2dthelperTest.php
@@ -18,6 +18,7 @@ class px2px2dthelperTest extends PHPUnit_Framework_TestCase{
 	 */
 	public function setup(){
 		// $this->fs = new \tomk79\filesystem();
+		require_once(__DIR__.'/../php/simple_html_dom.php');
 	}
 
 	/**
@@ -26,12 +27,8 @@ class px2px2dthelperTest extends PHPUnit_Framework_TestCase{
 	public function testPing(){
 
 		// ping打ってみる
-		$output = $this->passthru( [
-			'php',
-			__DIR__.'/testData/standard/.px_execute.php' ,
-			'/?PX=px2dthelper.ping' ,
-		] );
-
+		$output = $this->passthru( ['php', __DIR__.'/testData/standard/.px_execute.php', '/?PX=px2dthelper.ping' ] );
+		// var_dump($output);
 		$this->assertEquals( 'ok', trim($output) );
 
 	}//testPing()
@@ -81,6 +78,46 @@ class px2px2dthelperTest extends PHPUnit_Framework_TestCase{
 		] );
 
 	}//testBuild()
+
+	/**
+	 * TableAPIのテスト
+	 */
+	public function testTableApi(){
+
+		// Excelをtableに変換させる
+		// 存在しないファイルパスを渡したら、falseが返る。
+		$output = $this->passthru( ['php', __DIR__.'/testData/standard/.px_execute.php', '/?PX=px2dthelper.convert_table_excel2html&path='.urlencode(__DIR__.'/testData/xlsx/default.xlsx') ] );
+		$output = json_decode($output);
+		// var_dump($output);
+		$this->assertEquals( gettype(''), gettype($output) );
+
+		$html = \tomk79\pickles2\px2dthelper\str_get_html(
+			$output ,
+			true, // $lowercase
+			true, // $forceTagsClosed
+			DEFAULT_TARGET_CHARSET, // $target_charset
+			false, // $stripRN
+			DEFAULT_BR_TEXT, // $defaultBRText
+			DEFAULT_SPAN_TEXT // $defaultSpanText
+		);
+
+		$this->assertEquals( 18, count($html->find('tr')) );
+		$this->assertEquals( 5, count($html->find('tr',0)->find('td')) );
+		$this->assertEquals( 'E18', $html->find('tr',17)->childNodes(4)->innertext );
+		$this->assertNull( $html->find('tr',18) );
+		$this->assertNull( $html->find('tr',17)->childNodes(5) );
+
+
+
+		// Excelをtableに変換させる
+		// 存在しないファイルパスを渡したら、falseが返る。
+		$output = $this->passthru( ['php', __DIR__.'/testData/standard/.px_execute.php', '/?PX=px2dthelper.convert_table_excel2html&path=' ] );
+		// var_dump($output);
+		$output = json_decode($output);
+		$this->assertFalse( $output );
+
+	}//testPing()
+
 
 
 
