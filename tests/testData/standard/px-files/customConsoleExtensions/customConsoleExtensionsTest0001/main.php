fff14adede406cc1080ae1f17ca05348fe828d1f
Tomoya Koyanagi <tomk79@gmail.com>
2021-01-04 21:23:47 +0900

    <customConsoleExt> API仕様を検討中 -> クライアントサイドスクリプトの情報を示す関数と GPI を追加

--- tests/testData/standard/px-files/customConsoleExtensions/customConsoleExtensionsTest0001/main.php
+++ tests/testData/standard/px-files/customConsoleExtensions/customConsoleExtensionsTest0001/main.php
@@ -1,10 +1,51 @@
 <?php
 class customConsoleExtensionsTest0001{
-    private $px;
-    public function __construct($px){
-        $this->px = $px;
-    }
-    public function get_label(){
-        return '拡張機能0001';
-    }
+
+	/** $px */
+	private $px;
+
+	/**
+	 * Constructor
+	 */
+	public function __construct($px){
+		$this->px = $px;
+	}
+
+	/**
+	 * 機能拡張の名前を取得する
+	 */
+	public function get_label(){
+		return '拡張機能0001';
+	}
+
+	/**
+	 * 機能拡張のクライアントサイド資材のベースディレクトリパスを取得する
+	 */
+	public function get_client_resource_base_dir(){
+		return __DIR__.'/resources/';
+	}
+
+	/**
+	 * 機能拡張のクライアントサイド資材一覧を取得する
+	 */
+	public function get_client_resource_list(){
+		$rtn = array();
+		$rtn['css'] = array('styles/cce0001.css');
+		$rtn['js'] = array('styles/cce0001.js');
+		return $rtn;
+	}
+
+	/**
+	 * クライアントサイドの初期化関数名を取得する
+	 */
+	public function get_client_initialize_function(){
+		return 'window.customConsoleExtensionsTest0001';
+	}
+
+	/**
+	 * General Purpose Interface
+	 */
+	public function gpi( $request ){
+		return false;
+	}
 }
---
+++ tests/testData/standard/px-files/customConsoleExtensions/customConsoleExtensionsTest0001/resources/scripts/cce0001.js
@@ -0,0 +1,3 @@
+window.customConsoleExtensionsTest0001 = function(){
+	alert('customConsoleExtensionsTest0001()');
+}
