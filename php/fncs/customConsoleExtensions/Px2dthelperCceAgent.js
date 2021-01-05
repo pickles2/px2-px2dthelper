cdfa1da071c7fa73609c3f170ec6405047687ba0
Tomoya Koyanagi <tomk79@gmail.com>
2021-01-05 22:58:58 +0900

    <customConsoleExt> cceAgent() のオプションに elm を追加

--- php/fncs/customConsoleExtensions/Px2dthelperCceAgent.js
+++ php/fncs/customConsoleExtensions/Px2dthelperCceAgent.js
@@ -1,7 +1,9 @@
 window.Px2dthelperCceAgent = function(options){
 	options = options || {};
+	options.elm = options.elm || false;
 	options.lang = options.lang || 'ja';
 	options.gpiBridge = options.gpiBridge || function(request, callback){};
+	this.elm = function(){return options.elm;}
 	this.lang = function(){return options.lang;}
 	this.gpi = function(request, callback){
 		options.gpiBridge(request, callback);
