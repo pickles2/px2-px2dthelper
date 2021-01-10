5f6930f580c04f4399c2d5e98ecfcb8165058c66
Tomoya Koyanagi <tomk79@gmail.com>
2021-01-10 22:58:43 +0900

    <customConsoleExt> GPIのコールバックに、 `res.response` のみを返すように修正
    拡張機能の開発者への直感的な反応を重視して。

--- php/fncs/customConsoleExtensions/Px2dthelperCceAgent.js
+++ php/fncs/customConsoleExtensions/Px2dthelperCceAgent.js
@@ -6,6 +6,15 @@ window.Px2dthelperCceAgent = function(options){
 	this.elm = function(){return options.elm;}
 	this.lang = function(){return options.lang;}
 	this.gpi = function(request, callback){
-		options.gpiBridge(request, callback);
+		options.gpiBridge(request, function(res){
+			if(!res){
+				console.error('GPI returns:', res);
+				return;
+			}
+			if(!res.result){
+				console.error('GPI Error:', res.message);
+			}
+			callback(res.response);
+		});
 	}
 }
