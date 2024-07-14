6662db5ad2f30a3eb8d2ba7ecdc7ea7849dba8c9
Tomoya Koyanagi <tomk79@gmail.com>
2024-07-14 12:51:05 +0900

    cceのコールバック関数が指定されていないときに、console.errorを表示するようにした。

--- php/fncs/customConsoleExtensions/Px2dthelperCceAgent.js
+++ php/fncs/customConsoleExtensions/Px2dthelperCceAgent.js
@@ -3,10 +3,22 @@ window.Px2dthelperCceAgent = function(options){
 	options = options || {};
 	options.elm = options.elm || false;
 	options.lang = options.lang || 'ja';
-	options.gpiBridge = options.gpiBridge || function(){};
-	options.onEditContent = options.onEditContent || function(){};
-	options.onEditThemeLayout = options.onEditThemeLayout || function(){};
-	options.onOpenInBrowser = options.onOpenInBrowser || function(){};
+	options.gpiBridge = options.gpiBridge || function(request, callback){
+		console.error('This app is not support `gpi()`.');
+	};
+	options.onPxCmd = options.onPxCmd || function(path, params, options, callback){
+		console.error('This app is not support `pxCmd()`.');
+	};
+	options.onEditContent = options.onEditContent || function(target){
+		console.error('This app is not support `editContent()`.');
+	};
+	options.onEditThemeLayout = options.onEditThemeLayout || function(target){
+		console.error('This app is not support `editThemeLayout()`.');
+	};
+	options.onOpenInBrowser = options.onOpenInBrowser || function(path){
+		console.error('This app is not support `openInBrowser()`.');
+	};
+
 	this.elm = function(){return options.elm;}
 	this.lang = function(){return options.lang;}
 	this.gpi = function(request, callback){
